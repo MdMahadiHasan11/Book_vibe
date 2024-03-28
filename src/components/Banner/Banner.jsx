ef8ff08e5eef6d5be2e5d2205e8b9cbbb7c13f74
MdMahadiHasan11 <hasanpustcse11@gmail.com>
2024-03-28 21:53:29 +0600

    complete

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -3,8 +3,8 @@ import { NavLink } from "react-router-dom";
 
 const Banner = () => {
     return (
-        <div className="hero min-h-screen bg-base-200">
-  <div className="hero-content flex-col lg:flex-row-reverse">
+        <div className="rounded-2xl hero min-h-screen bg-base-200">
+  <div className=" hero-content flex-col lg:flex-row-reverse">
     <img src={bannerImg} />
     <div>
       <h1 className="lg:text-5xl md:text-3xl text-2xl font-bold lg:w-3/5">Books to freshen up your bookshelf</h1>
